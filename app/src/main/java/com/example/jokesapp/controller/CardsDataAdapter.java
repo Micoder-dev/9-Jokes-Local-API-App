58ae5f938ed890d00a65de39ab0793449424800c
Micoder-dev <micoder.com@gmail.com>
2022-07-23 20:19:42 +0530

    Simple Like & Dislike feature added

--- app/src/main/java/com/example/jokesapp/controller/CardsDataAdapter.java
+++ app/src/main/java/com/example/jokesapp/controller/CardsDataAdapter.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -12,8 +14,14 @@ import com.example.jokesapp.R;
 
 public class CardsDataAdapter extends ArrayAdapter<String> {
 
+    private Context mContext;
+    private boolean clicked = true;
+
     public CardsDataAdapter(@NonNull Context context, int resource) {
         super(context, resource);
+
+        mContext =context;
+
     }
 
     @Override
@@ -21,6 +29,30 @@ public class CardsDataAdapter extends ArrayAdapter<String> {
         //supply the layout for your card
         TextView v = (contentView.findViewById(com.example.jokesapp.R.id.content));
         v.setText(getItem(position));
+
+        ImageButton likeButton = contentView.findViewById(R.id.likeButton);
+
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Toast.makeText(mContext, "Like btn is tapped", Toast.LENGTH_SHORT).show();
+
+                if (clicked) {
+
+                    likeButton.setImageResource(R.drawable.like_filled);
+                    clicked = false;
+
+                } else {
+
+                    likeButton.setImageResource(R.drawable.like_empty);
+                    clicked = true;
+
+                }
+
+            }
+        });
+
         return contentView;
     }
 
